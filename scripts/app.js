f8f7b53a53ce55974f33ac31597eb8d9a75cb95f
Jason Tham <jtham@sbcglobal.net>
2015-10-29 16:22:35 -0500

    added window loading logic

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,12 @@ window.onload = function() {
 
 statusReport('hey Hey HEY!!!!');
 
+var username = prompt('What is your name?');
+var mission = prompt('What is your mission?');
+var contentContainer = document.getElementById('content');
+
+contentContainer.innerHTML = username + ' is on a mission to ' + mission;
+
 }
 
 function statusReport(message){
